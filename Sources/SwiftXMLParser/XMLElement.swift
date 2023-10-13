2702509a28b1f93ad8d6a1f0f56183924046baa2
Maciej Kuliszkiewicz <mbanasiewicz@gmail.com>
2023-10-13 09:06:04 +0200

    Update XMLElement.swift

--- Sources/SwiftXMLParser/XMLElement.swift
+++ Sources/SwiftXMLParser/XMLElement.swift
@@ -1,4 +1,4 @@
-// Copyright © 2022 Maciej Banasiewicz. All rights reserved.
+// Copyright © 2022 Maciej Kuliszkiewicz. All rights reserved.
 
 public final class XMLElement: Hashable {
     public let name: String
